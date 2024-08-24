76f287273a92a7a51b48fe654d2b9dd30fd398b3
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-08-23 21:36:32 -0300

    feat: add `onError` option

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ import { AuthenticateOptions, Strategy, StrategyVerifyCallback } from "remix-aut
 import SteamAPI, { UserSummary } from "steamapi";
 
 export interface SteamStrategyOptions {
-    returnURL: string;
-    realm?: string;
     apiKey: string;
+    onError?: (error: unknown) => void;
+    realm?: string;
+    returnURL: string;
 }
 
 export type SteamStrategyVerifyParams = {
@@ -64,8 +65,13 @@ export class SteamStrategy<User> extends Strategy<User, SteamStrategyVerifyParam
     }
 
     async authenticate(request: Request, sessionStorage: SessionStorage, options: AuthenticateOptions): Promise<User> {
-        const { apiKey, returnURL, realm } =
+        const { apiKey, onError, returnURL, realm } =
             typeof this.options === "function" ? await this.options(request) : this.options;
+        const notifyError = (error: unknown) => {
+            if (!(error instanceof Response)) {
+                onError?.(error);
+            }
+        };
         const relyingParty = new OpenID.RelyingParty(returnURL, realm ?? null, true, false, []);
         const steamApi = new SteamAPI(apiKey);
         try {
@@ -78,11 +84,18 @@ export class SteamStrategy<User> extends Strategy<User, SteamStrategyVerifyParam
                 const user = await this.verify({ user: steamUserSummary, request });
                 return this.success(user, request, sessionStorage, options);
             } catch (error) {
+                notifyError(error);
                 let message = (error as Error).message;
                 return this.failure(message, request, sessionStorage, options);
             }
-        } catch {
-            throw redirect(await authenticateToSteam(relyingParty));
+        } catch (error) {
+            notifyError(error);
+            try {
+                throw redirect(await authenticateToSteam(relyingParty));
+            } catch (error) {
+                notifyError(error);
+                throw error;
+            }
         }
     }
 }
