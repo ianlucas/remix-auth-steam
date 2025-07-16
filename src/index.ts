c26542a29c3f48a3018da00caf12b104f540c15a
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-07-16 01:43:54 -0300

    feat: port SteamOpenID.php to the library (#3)

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,18 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import OpenID from "openid";
 import { redirect } from "react-router";
 import { Strategy } from "remix-auth/strategy";
-import SteamAPI, { type UserSummary } from "steamapi";
+import { SteamOpenID } from "./openid.js";
 
 namespace SteamStrategy {
     export interface Options {
-        apiKey: string;
-        onError?: (error: unknown) => void;
-        realm?: string;
         returnURL: string;
     }
 
     export type VerifyOptions = {
         request: Request;
-        user: UserSummary;
+        userID: string;
     };
 }
 
@@ -32,72 +28,17 @@ export class SteamStrategy<User> extends Strategy<User, SteamStrategy.VerifyOpti
         super(verify);
     }
 
-    private authenticateToSteam(relyingParty: OpenID.RelyingParty): Promise<string> {
-        return new Promise((resolve, reject) =>
-            relyingParty.authenticate("https://steamcommunity.com/openid", false, (err, url) => {
-                if (err) {
-                    return reject(err);
-                }
-                if (!url) {
-                    return reject("Got no URL from authenticate method.");
-                }
-                return resolve(url);
-            })
-        );
-    }
-
-    private verifySteamAssertion(
-        relyingParty: OpenID.RelyingParty,
-        request: Request
-    ): Promise<{
-        authenticated: boolean;
-        claimedIdentifier?: string | undefined;
-    }> {
-        return new Promise((resolve, reject) =>
-            relyingParty.verifyAssertion(request, (err, result) => {
-                if (err) {
-                    return reject(err);
-                }
-                if (!result) {
-                    return reject("No result from verifyAssertion.");
-                }
-                return resolve(result);
-            })
-        );
-    }
-
     async authenticate(request: Request): Promise<User> {
-        const { apiKey, onError, returnURL, realm } =
-            typeof this.options === "function" ? await this.options(request) : this.options;
-        const handleError = (error: unknown) => {
-            if (!(error instanceof Response)) {
-                onError?.(error);
-            }
-        };
-        try {
-            const relyingParty = new OpenID.RelyingParty(returnURL, realm ?? null, true, false, []);
-            const steamApi = new SteamAPI(apiKey);
-            const url = new URL(request.url);
-            const callbackUrl = new URL(returnURL);
-            if (url.pathname === callbackUrl.pathname) {
-                const result = await this.verifySteamAssertion(relyingParty, request);
-                if (!result.authenticated || !result.claimedIdentifier) {
-                    throw new Error("Not authenticated from result.");
-                }
-                const userId = result.claimedIdentifier.toString().split("/").at(-1);
-                if (userId === undefined) {
-                    throw new Error("Unable to get SteamID.");
-                }
-                return await this.verify({
-                    user: (await steamApi.getUserSummary(userId)) as UserSummary,
-                    request
-                });
-            } else {
-                throw redirect(await this.authenticateToSteam(relyingParty));
-            }
-        } catch (error) {
-            handleError(error);
-            throw error;
+        const { returnURL } = typeof this.options === "function" ? await this.options(request) : this.options;
+        const steamOpenID = new SteamOpenID(returnURL, request);
+        if (steamOpenID.shouldValidate()) {
+            const userID = await steamOpenID.validate();
+            return await this.verify({
+                userID,
+                request
+            });
+        } else {
+            throw redirect(steamOpenID.getAuthUrl());
         }
     }
 }
